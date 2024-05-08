3998f608987f5fd0e595759c1fcf18f6763dfe75
NickEsColR <nicoesol0818@gmail.com>
2024-05-08 10:27:20 -0500

    feat: add dispatch when login

--- src/store/auth/authSlice.js
+++ src/store/auth/authSlice.js
@@ -20,13 +20,16 @@ export const authSlice = createSlice({
             state.user = payload;
             state.errorMessage = undefined;
         },
-        onLogout: (state) => {
+        onLogout: (state, {payload}) => {
             state.status = 'not-authenticated';
             state.user = {};
-            state.errorMessage = undefined;
+            state.errorMessage = payload;
         },
+        clearErrorMessage: (state) => {
+            state.errorMessage = undefined;
+        }
     },
 });
 
-export const {onChecking,onLogin,onLogout} = authSlice.actions;
+export const {onChecking,onLogin,onLogout,clearErrorMessage} = authSlice.actions;
 
